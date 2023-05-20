651c285f24d2c73fd5bf6d63a3c3ed84e818c99e
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2023-05-20 14:27:30 +0100

    + Drugs are animated

---
+++ KillerTrip/Assets/KillerTrip/Scripts/Objectives/ObjectiveTypes/LayerObjective.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerObjective : ObjectivesManager
+{
+    [SerializeField] private string _objectiveTitle;
+    [SerializeField] private List<GameObject> _targetObjects = new List<GameObject>();
+    [SerializeField] private bool _isConsecutiveObjective = false;
+
+    private ObjectivesManager _objectivesManager;
+
+    void Start()
+    {
+        _objectivesManager = transform.parent.GetComponent<ObjectivesManager>();
+
+        _objectiveTitleText.text = _objectiveTitle;
+        _objectiveQuantityText.text = _targetObjects.Count.ToString();
+    }
+
+    void Update()
+    {
+        if (_targetObjects.Count <= 0)
+        {
+            if (_isConsecutiveObjective)
+                _objectivesManager.ObjectiveIsComplete();
+            else
+                gameObject.SetActive(false);
+        }
+        else
+            _objectiveQuantityText.text = _targetObjects.Count.ToString();
+    }
+}
